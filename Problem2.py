28e8aef59dd825a2fc8e3caa93c3dfcf87f89c9d
Megha Raykar <meghamanjunathraykar@Meghas-MacBook-Air.local>
2026-03-06 17:44:03 -0800

    Added Problem2

---
+++ Problem2.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/symmetric-tree/description/
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        q = deque()
+        q.append(root.left)
+        q.append(root.right)
+
+        while len(q) > 0:
+            left = q.popleft()
+            right = q.popleft()
+
+            if left is None and right is None:
+                continue
+            elif left is None or right is None:
+                return False
+            elif left.val != right.val:
+                return False
+            
+            q.append(left.left)
+            q.append(right.right)
+            q.append(left.right)
+            q.append(right.left)
+
+        return True
